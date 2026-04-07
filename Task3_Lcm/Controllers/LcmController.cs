f15b8925080327314a2c7eea192b4e780c3d778a
ebrar.guzel <ebrar.guzel26@gmail.com>
2026-04-08 00:16:31 +0500

    First Commit for Task3

---
+++ Task3_Lcm/Controllers/LcmController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Task3_Lcm.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LcmController : ControllerBase
+    {
+        [HttpGet]
+        public string Get(string x, string y)
+        {
+            if (!int.TryParse(x, out int a) || !int.TryParse(y, out int b) || a < 0 || b < 0)
+                return "NaN";
+
+            int Gcd(int m, int n)
+            {
+                while (n != 0)
+                {
+                    int temp = n;
+                    n = m % n;
+                    m = temp;
+                }
+                return m;
+            }
+
+            int lcm = (a == 0 || b == 0) ? 0 : (a * b) / Gcd(a, b);
+            return lcm.ToString();
+        }
+    }
+}
